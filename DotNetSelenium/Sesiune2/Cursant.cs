2d47637f0fd5ad56b7fa4ea0b6a0d66f8c80c285
julia varga <varga_jh@yahoo.com>
2024-11-28 13:38:26 +0200

    Add project files.

---
+++ DotNetSelenium/Sesiune2/Cursant.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetSelenium.Sesiune2
+{
+    public class Cursant
+    {
+        string numeCursant;
+        string prenumeCursant;
+        int varsta;
+        int cnp;
+        bool esteActiv;
+        char gen;
+        float nota;
+        double inaltime;
+
+        [Test]
+        public void metodaTest()
+        {
+            numeCursant = "varga";
+            prenumeCursant = "julia";
+            varsta = 20;
+          //  cnp = 281818578462;
+            var numarLung = 281818578462;
+            esteActiv = false;
+            gen = 'M';
+            nota = 9.50F;
+            inaltime = 1.63;
+
+            Console.Write("Numele: " + numeCursant + "\n" );
+            Console.WriteLine("Prenumele: " + prenumeCursant);
+            Console.WriteLine("Varsta: " + numarLung);
+         
+
+        }
+
+    }
+}
