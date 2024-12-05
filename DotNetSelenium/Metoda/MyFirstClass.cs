44d383ceea084b496dc5a393d7bbe8a08d70836c
julia varga <varga_jh@yahoo.com>
2024-12-05 16:08:40 +0200

    Tema2

---
+++ DotNetSelenium/Metoda/MyFirstClass.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetSelenium.Metoda
+{
+    public class MyFirstClass
+    {
+        [Test]
+        public void metodaTestNumbers()
+        {
+           // sumNumbers();
+           // sumNumbers(1, 2);
+           // sumNumbers(1, 2, 3);
+          
+          int sum = GetResult(1, 2, 3);
+          Console.WriteLine("Afisa suma: " + sum);
+
+
+        }
+
+        public void sumNumbers()
+        {
+            int number1 = 5;
+            int number2 = 7;
+            int sum = number1 + number2;
+            Console.WriteLine("Rezultatul adunarii este " + sum);
+
+        }
+
+        public void sumNumbers(int number1, int number2)
+        {
+            int sum = number1 + number2;
+            Console.WriteLine("Rezultatul adunarii este " + sum);
+        }
+
+        public void sumNumbers(int number1, int number2, int number3)
+        {
+            int sum = number1 + number2 + number3;
+            Console.WriteLine("Rezultatul adunarii este " + sum);
+        }
+
+        public int GetResult(int number1, int number2, int number3)
+        {
+            int sum = number1 + number2 + number3;
+            return sum;
+        }
+
+    }
+
+   
+}
---
+++ DotNetSelenium/Practice/Persoana.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetSelenium.Practice
+{
+
+
+    public static class Persoana
+    {
+        public static string FirstName { get; set; } = "Julia";
+        public static string LastName { get; set; } = "Varga";
+        public static string Gender { get; set; } = "F";
+        public static int Age { get; set; } = 35;
+        public static int YearsToAdd { get; set; } = 2;
+        
+        public static int CalculateAge(int YearsToAdd)
+        {
+            int CalculateAge = YearsToAdd + Age;
+            return CalculateAge;
+        }       
+
+        public static void DisplayUserDetails(string FirstName, string LastName, string Gender, int Age)
+        {
+            int FutureAge = CalculateAge(YearsToAdd);
+
+            Console.WriteLine("The person's details are as follows: ");
+            Console.WriteLine("- First Name: " + FirstName);
+            Console.WriteLine("- Last name: " + LastName);
+            Console.WriteLine("- Gender: " + Gender);
+            Console.WriteLine(value: "You will be " + FutureAge + " years old in " + YearsToAdd + " years!");
+        }
+
+
+        [Test]
+        public static void DisplayPerson()
+        {
+            DisplayUserDetails(FirstName, LastName, Gender, Age);
+        }
+
+    }
+ }
