3ffd3f8a7b42b0365cf096c9a07d96209961f319
julia varga <varga_jh@yahoo.com>
2025-01-20 10:29:22 +0200

    tema structuri repetitive

--- DotNetSelenium/Practice/Persoana.cs
+++ DotNetSelenium/Practice/Persoana.cs
@@ -4,10 +4,11 @@ using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+using NUnit.Framework.Interfaces;
 
 namespace DotNetSelenium.Practice
 {
-
 
     public static class Persoana
     {
@@ -16,31 +17,41 @@ namespace DotNetSelenium.Practice
         public static string gender { get; set; } = "Other";
         public static int age { get; set; } = 98;
         public static int yearsToAdd { get; set; } = 2;
+        public static List<string> Skills { get; set; } = new List<string>()
+        {
+            "Cooking",
+            "Photography",
+            "Progaming",
+            "Testing",
+            "Ninja",
+            "Swimming",
+            "CIA",
+            "Dancing",
+            "Singing"
+        };
 
-      
 
 
         public static int CalculateAge(int yearsToAdd)
         {
             int CalculateAge = yearsToAdd + age;
             return CalculateAge;
-        }       
+        }
 
         public static void DisplayUserDetails(string firstName, string lastName, string gender, int age)
         {
             int FutureAge = CalculateAge(yearsToAdd);
 
-            
+
             Console.WriteLine("The person's details are as follows: ");
             Console.WriteLine("- First Name: " + firstName);
             Console.WriteLine("- Last name: " + lastName);
             Console.WriteLine("- Gender: " + gender);
             Console.WriteLine(value: "You will be " + FutureAge + " years old in " + yearsToAdd + " years!");
-           // Console.WriteLine("\n");
+            // Console.WriteLine("\n");
             Console.WriteLine(IsOld(age));
             ShowGender(gender);
-
-
+            
         }
 
 
@@ -49,24 +60,24 @@ namespace DotNetSelenium.Practice
 
             return GetAge(age);
 
-         }
+        }
 
         private static string GetAge(int age)
-            {
+        {
 
-                if (age >= 100)
-                {
-                    return "You are ancient!";
-                }
-                else if (age >= 50)
-                {
-                    return "You are still old!";
-                }
-                else
-                {
-                   return "You are still young!";
-                }
+            if (age >= 100)
+            {
+                return "You are ancient!";
             }
+            else if (age >= 50)
+            {
+                return "You are still old!";
+            }
+            else
+            {
+                return "You are still young!";
+            }
+        }
 
 
         public static void ShowGender(string gender)
@@ -89,15 +100,69 @@ namespace DotNetSelenium.Practice
         }
 
 
+        public static void AddSkills(string skill)
+        {
+            Skills.Add(skill);
+            Console.WriteLine($"Added a new skill: {skill}");
 
+        }
 
+       public static void DisplaySkill()
+        {
+
+            List<string> displayListFilter = new List<string>();
+                 
+            foreach (var skill in Skills)
+            {
+
+                Console.WriteLine($"Added a new skill: {skill}");
+                
+
+                if (skill != "Ninja" && skill != "CIA")
+                {
+                    displayListFilter.Add(skill);
+                }
+
+                else if (skill == "Ninja")
+                {
+                    continue;
+                }
+
+                else if (skill == "CIA")
+                {
+                    Console.WriteLine(">>>>>Classified information, no further skills are displayed!<<<<<");
+                    break;
+                }
+
+                displayListFilter.Add(skill);
+
+               
+            }
+            string skillJoin = string.Join("; ", displayListFilter);
+            Console.WriteLine("The person has the following skills:  \n" + skillJoin);
+
+        }
 
         [Test]
         public static void DisplayPerson()
         {
-              DisplayUserDetails(firstName, lastName, gender, age);
-            //Console.WriteLine(IsOld(age));
-           //ShowGender(gender);
+            //  DisplayUserDetails(firstName, lastName, gender, age);
+            //  Console.WriteLine(IsOld(age));
+            //  ShowGender(gender);
+
+
+            //AddSkills("Cooking1");
+            //AddSkills("Photography");
+            //AddSkills("Progaming");
+            //AddSkills("Testing");
+            //AddSkills("Ninja");
+            //AddSkills("Swimming");
+            //AddSkills("CIA");
+            //AddSkills("Dancing");
+            //AddSkills("Singing");
+
+            DisplaySkill();
+             
 
         }
 
@@ -107,7 +172,7 @@ namespace DotNetSelenium.Practice
 
 
     }
- }
+}
 
 
 
---
+++ DotNetSelenium/StructuriRepetitive/StructuriRepetitive.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetSelenium.StructuriRepetitive
+{
+    public class StructuriRepetitive
+    {
+
+        [Test]
+        public void Test()
+        {
+
+
+            // for(int i= 1; i<=7; i++)
+            // {
+            //     Console.WriteLine(i);
+            // }
+
+
+            /* int i = 1;
+
+             for(; i<=8; i++)
+             {
+                 Console.WriteLine(i);
+
+             }*/
+
+            /*int i = 0;
+
+            for (; i <= 7; )
+            {
+                i++;
+                Console.WriteLine(i);
+                
+            }*/
+
+            /* string[] numeColegi = new string[3];
+             numeColegi[0] = "A";
+             numeColegi[1] = "B";
+             numeColegi[2] = "C";
+
+             for (int i = 0; i < numeColegi.Length; i++)
+             {
+                 Console.WriteLine(numeColegi[i]);
+             }*/
+
+            List<string> numeColegi = new List<string>
+            {
+               "A",
+               "B", 
+               "C"
+            };
+
+            foreach (string numeColeg in numeColegi)
+            {
+                Console.WriteLine(numeColeg);
+            }
+
+            
+
+
+        }
+
+
+
+
+
+    }
+}
