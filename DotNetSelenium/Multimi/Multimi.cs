bdff3eed4abb72706a823aa63ec4c9f6a7dbe45b
julia varga <varga_jh@yahoo.com>
2025-02-17 17:36:33 +0200

    tema arrays & lists

---
+++ DotNetSelenium/Multimi/Multimi.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetSelenium.Multimi
+{
+    public class Multimi
+    {
+        [Test]
+        public void Test1()
+        {
+
+            Dictionary<string, string> objects = new Dictionary<string, string>
+            {
+                {"object", "laptop" },
+                {"fruit", "apple" },
+                {"sport", "snowboard" },
+            };
+
+            foreach (var obj in objects)
+            {
+                Console.WriteLine($"The key is {obj.Key} ");
+                Console.WriteLine($"The value is {obj.Value} ");
+            }
+
+            Dictionary<string, List<string>> classification = new Dictionary<string, List<string>>();
+            List<string> citiesOfRomania = new List<string> { "Bucuresti", "Cluj-Napoca", "Timisoara" };
+            List<string> citiesOfFrance = new List<string> { "Paris", "Lyon", "Marseille" };
+            List<string> citiesOfGermany = new List<string> { "Berlin", "Munich", "Hamburg" };
+
+           // classification.Add("Romania", citiesOfRomania);
+            classification["Romania"] = citiesOfRomania;
+            classification["France"] = citiesOfFrance;
+            classification["Germany"] = citiesOfGermany;
+
+            foreach (var key in classification.Keys)
+            {
+                Console.WriteLine($"The country is {key}");
+                Console.WriteLine($"The city are " + string.Join(",", classification[key]));
+            }
+
+
+
+
+            //foreach (var key in classification)
+            //{
+            //    foreach(var value in key.Value)
+            //    {
+            //        Console.WriteLine($"The country is {key.Key}");
+            //        Console.WriteLine($"The city is {value} ");
+            //    }
+
+            //}
+        }
+
+
+    }
+}
---
+++ DotNetSelenium/Practice/PracticeArraysLists.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetSelenium.Practice
+{
+    public class PracticeArraysLists
+    {
+        [Test]
+
+
+        public void InputNumber()
+        {
+            ListNumber(6);
+        }
+
+
+        public void ListNumber(int n)
+        {
+
+            Console.WriteLine($"Please input a number for list: {n}");
+
+            List<int> numbers = new List<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                numbers.Add(i);
+              
+            }
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                Console.WriteLine($"Your list contains the following items [{i}]: {numbers[i]} " );
+
+            }
+
+
+            Console.WriteLine("Your list contains the following items: " + string.Join(", ", numbers));
+
+
+            List<int> evenNumbers = new List<int>();
+
+            List<int> oddNumbers = new List<int>();
+
+            foreach (int i in numbers)
+            {
+                if (i % 2 == 0)
+                {
+                    evenNumbers.Add(i);
+                }
+                else
+                {
+                    oddNumbers.Add(i);
+                }
+            }
+
+
+            Console.WriteLine("The list of even numbers is: " + string.Join(", ", evenNumbers));
+            Console.WriteLine("The list of odd numbers is: " + string.Join(", ", oddNumbers));
+
+            int evenSum = 0;
+            int oddSum = 0;
+
+            foreach (int i in evenNumbers)
+            {
+                evenSum += i;
+            }
+
+            foreach (int i in oddNumbers)
+            {
+                oddSum += i;
+            }
+
+
+            Console.WriteLine("The sum of even numbers is: " + evenSum);
+            Console.WriteLine("The sum of odd numbers is: " + oddSum);
+
+        }
+
+
+    }
+}
