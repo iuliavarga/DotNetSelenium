d7ec4d84fdbf9415dbc1a851779c96c48f503640
julia varga <varga_jh@yahoo.com>
2025-04-08 14:41:24 +0300

    curs 1 interm.

---
+++ DotNetSelenium/OOP/Car.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetSelenium.OOP
+{
+    public class Car
+    {
+        string engine;
+        string color;
+        int doors;
+        double cilinder;
+        string gearboxtype;
+
+        public Car(string engine, string color, int doors, double cilinder)
+        {
+            this.engine = engine;
+            this.color = color;
+            this.doors = doors;
+            this.cilinder = cilinder;
+        }
+
+        public Car(string engine, string color, int doors, double cilinder, string gearboxtype)
+        {
+            this.engine=engine;
+            this.color = color;
+            this.doors = doors;
+            this.cilinder = cilinder;
+            this.gearboxtype = gearboxtype;
+        }
+
+        public void DisplayData()
+        {
+            Console.WriteLine($"Engine is: {engine}");
+            Console.WriteLine($"Color is: {color}");
+            Console.WriteLine($"Number of doors is: {doors}");
+            Console.WriteLine($"Cilinder is: {cilinder}");
+
+            if(gearboxtype != null)
+            {
+                Console.WriteLine($"Gearbox type is: {gearboxtype}");
+            }
+
+           
+
+        }
+    }
+}
---
+++ DotNetSelenium/OOP/CarTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetSelenium.OOP
+{
+    public class CarTest
+    {
+
+        [Test]
+        public void MetodaTest()
+        {
+            var car1 = new Car("benzina", "rosie", 5, 1.3);
+            car1.DisplayData();
+
+            Console.WriteLine();
+
+            Car car2 = new Car("motorina", "albastra", 2, 2.0);
+            car2.DisplayData();
+
+            Console.WriteLine();
+
+            Car car3 = new Car("benzina", "alb", 4, 1.4, "automata");
+            car3.DisplayData();
+
+            
+        }
+    }
+}
